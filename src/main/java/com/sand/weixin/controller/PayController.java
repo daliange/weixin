7d74efa0c375295f1cf97af0718dde276e654d96
daliange <778425935@qq.com>
2017-05-04 10:12:44 +0800

    11

--- src/main/java/com/sand/weixin/controller/PayController.java
+++ src/main/java/com/sand/weixin/controller/PayController.java
@@ -58,29 +58,7 @@ public class PayController {
 	}
 	
 	
-	@RequestMapping(value="wxReturn")
-	public String wxReturn(HttpServletRequest request,HttpServletResponse response) {
-		
-		String code=request.getParameter("code");
-		String state=request.getParameter("state");
-		logger.info("微信返回[code:"+code +  " state:"+state+" ]");
-		
-		Map<String, Object> param=new HashMap<String, Object>();
-		param.put("appid", "wx94348ceda2791351");
-		//wx94348ceda2791351 生产  杉德支付
-		//wx71bc1338e63671a1 测试
-		param.put("secret", "1aab250f4fee9d159a98fc730799b63f");
-		//1aab250f4fee9d159a98fc730799b63f  生产  支付
-		//716b62a1b63a6d9a447011f3e6567ad8
-		param.put("code", code);
-		param.put("grant_type", "authorization_code");
-		
-		String ret=HttpUtils.doPost("https://api.weixin.qq.com/sns/oauth2/access_token", param);	
-		logger.info("向微信申请access_token返回 " +  ret);
-		
-		return "/wx/payfor_weixin";
-		
-	}
+	
 	
 	
 	@RequestMapping(value="alipayReturn")
@@ -205,6 +183,31 @@ public class PayController {
 		}
 	
 		return credential;
+	}
+	
+	
+	@RequestMapping(value="wxReturn")
+	public String wxReturn(HttpServletRequest request,HttpServletResponse response) {
+		
+		String code=request.getParameter("code");
+		String state=request.getParameter("state");
+		logger.info("微信返回[code:"+code +  " state:"+state+" ]");
+		
+		Map<String, Object> param=new HashMap<String, Object>();
+		param.put("appid", "wx94348ceda2791351");
+		//wx94348ceda2791351 生产  杉德支付
+		//wx71bc1338e63671a1 测试
+		param.put("secret", "1aab250f4fee9d159a98fc730799b63f");
+		//1aab250f4fee9d159a98fc730799b63f  生产  支付
+		//716b62a1b63a6d9a447011f3e6567ad8
+		param.put("code", code);
+		param.put("grant_type", "authorization_code");
+		
+		String ret=HttpUtils.doPost("https://api.weixin.qq.com/sns/oauth2/access_token", param);	
+		logger.info("向微信申请access_token返回 " +  ret);
+		
+		return "/wx/payfor_weixin";
+		
 	}
 	
 	
